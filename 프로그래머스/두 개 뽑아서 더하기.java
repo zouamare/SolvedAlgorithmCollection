030f90929b3c8d84763b98b54b64f63c55e1a4bb
juha <zouamare@gmail.com>
2022-05-11 17:26:47 +0900

    프로그래머스 - 두 개 뽑아서 더하기

---
+++ 프로그래머스/두 개 뽑아서 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i=0; i<numbers.length; i++){
+            for(int j=0; j<numbers.length; j++){
+                if(i==j)
+                    continue;
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
