0e4229e720d43bd68e213ff51fdd289f328bf368
juha <zouamare@gmail.com>
2022-05-12 15:02:28 +0900

    프로그래머스 - 문자열 내 마음대로 정렬하기

---
+++ 프로그래머스/Level 1/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.charAt(n)>o2.charAt(n))
+                    return 1;
+                else if(o1.charAt(n)<o2.charAt(n))
+                    return -1;
+                else{
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+        return strings;
+    }
+}
