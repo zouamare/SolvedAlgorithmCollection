eefe7260eeb88b1f41d70f03ea2107d9737397b8
juha <zouamare@gmail.com>
2022-05-16 14:55:55 +0900

    2046. 스탬프 찍기

---
+++ SW Expert Academy/D1/2046. 스탬프 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i=0; i<T; i++){
+            System.out.print("#");
+        }
+    }
+}
