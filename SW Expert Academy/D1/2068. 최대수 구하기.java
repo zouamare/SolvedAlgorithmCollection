94d273ed43e70d2bb8e3c71b4e2cb3e03718145d
juha <zouamare@gmail.com>
2022-05-16 15:50:16 +0900

    SWEA - 2068. 최대수 구하기

---
+++ SW Expert Academy/D1/2068. 최대수 구하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int test_case = 1; test_case<=T; test_case++){
+            int max = Integer.MIN_VALUE;
+            for(int i = 0; i<10; i++){
+                max = Math.max(max,sc.nextInt());
+            }
+            System.out.println("#"+test_case+" "+max);
+        }
+    }
+}
