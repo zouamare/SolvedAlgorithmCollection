d077752aa5d515f2c76da44fb7c06b4834f58fdd
juha <zouamare@gmail.com>
2022-05-16 15:00:00 +0900

    SWEA - 2047. 신문 헤드라인

---
+++ SW Expert Academy/D1/2047. 신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+
+        System.out.println(S.toUpperCase());
+    }
+}
