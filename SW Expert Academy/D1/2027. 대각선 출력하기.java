99e2708a187b4d9e79a4f95daaf86fa141542995
juha <zouamare@gmail.com>
2022-05-16 14:45:37 +0900

    2027. 대각선 출력하기

---
+++ SW Expert Academy/D1/2027. 대각선 출력하기.java
@@ -0,0 +1,11 @@
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+    }
+}
