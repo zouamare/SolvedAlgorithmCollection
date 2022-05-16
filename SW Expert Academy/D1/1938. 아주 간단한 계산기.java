e88c9b1bd218de63739923d901f068abc5c8545f
juha <zouamare@gmail.com>
2022-05-16 14:35:57 +0900

    SWEA - 1938. 아주 간단한 계산기

---
+++ SW Expert Academy/D1/1938. 아주 간단한 계산기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+    }
+}
