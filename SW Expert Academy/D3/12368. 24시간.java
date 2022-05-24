af304fb316fd44ccc811a9b30762e98b972625e9
juha <zouamare@gmail.com>
2022-05-24 09:04:39 +0900

    SWEA - 12368. 24시간

---
+++ SW Expert Academy/D3/12368. 24시간.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println("#"+test_case+" "+(A+B)%24);
+        }
+    }
+}
