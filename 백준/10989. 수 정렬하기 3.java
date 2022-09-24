b11daa60df4a303a2231a2c1db1ca1eb7ddb1a0f
zouamare <zouamare@gmail.com>
2022-09-24 22:15:53 +0900

    백준 - 10989. 수 정렬하기 3

---
+++ 백준/10989. 수 정렬하기 3.java
@@ -0,0 +1,19 @@
+package coding_test.Algo20220924;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_10989_수정렬하기3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++)  arr[i] = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(arr);
+        for(int i = 0; i < N; i++)  sb.append(arr[i]).append("\n");
+        System.out.println(sb);
+    }
+}
