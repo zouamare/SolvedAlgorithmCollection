e06eab21fe2f733cb8eec69f5aed63ede4b7c6b4
zouamare <zouamare@gmail.com>
2022-10-01 23:13:21 +0900

    백준 - 10171. 고양이

---
+++ 백준/10171. 고양이.java
@@ -0,0 +1,14 @@
+package coding_test.Algo2022년_10월.day01;
+
+public class BOJ_10171_고양이 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\\    /\\").append("\n");
+        sb.append(" )  ( ')").append("\n");
+        sb.append("(  /  )").append("\n");
+        sb.append(" \\(__)|").append("\n");
+
+        System.out.println(sb);
+    }
+}
