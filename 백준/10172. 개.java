ca273db0a588f455557f1d0e8915f62e73135eed
zouamare <zouamare@gmail.com>
2022-10-09 15:48:30 +0900

    백준 - 10172. 개

---
+++ 백준/10172. 개.java
@@ -0,0 +1,13 @@
+package coding_test.Algo2022년_10월.day09;
+
+public class BOJ_10172_개 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|").append("\n");
+        sb.append("|q p|   /}").append("\n");
+        sb.append("( 0 )\"\"\"\\").append("\n");
+        sb.append("|\"^\"`    |").append("\n");
+        sb.append("||_/=\\\\__|").append("\n");
+        System.out.println(sb);
+    }
+}
