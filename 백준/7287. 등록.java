91a1293dd59affee3c3f08f011265b46682f81ee
zouamare <zouamare@gmail.com>
2022-10-14 23:41:19 +0900

    백준 - 7287. 등록

---
+++ 백준/7287. 등록.java
@@ -0,0 +1,8 @@
+package coding_test.Algo2022년_10월.day14;
+
+public class BOJ_7287_등록 {
+    public static void main(String[] args){
+        System.out.println("196");
+        System.out.println("zouamare");
+    }
+}
