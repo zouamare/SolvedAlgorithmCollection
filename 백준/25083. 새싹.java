191c37cd2defe4cca514e73def4bef02ba217f34
zouamare <zouamare@gmail.com>
2022-11-20 23:15:06 +0900

    백준 - 25083. 새싹

---
+++ 백준/25083. 새싹.java
@@ -0,0 +1,14 @@
+package coding_test.Algo2022년_11월.day20;
+
+public class BOJ_25083_새싹 {
+    public static void main(String[] args) {
+        //잔디 심는 것처럼 새싹도 심었다 :)
+        String s= "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.println(s);
+    }
+}
